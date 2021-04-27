c81d5353f58d63e36c8f9a5ef6c4df867e26447f
Hoon Kim <jhk4tt@virginia.edu>
2021-04-26 21:59:33 -0400

    Formats the manufacturer table

--- formex/ManufacturersSelect.php
+++ formex/ManufacturersSelect.php
@@ -8,13 +8,24 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$result = mysqli_query($con, "SELECT * FROM manufacturers ORDER BY name");
+$query = "SELECT * FROM manufacturers";
 
-while ($row = mysqli_fetch_array($result)) {
-    echo $row['man_id'];
-    echo " " . $row['name'];
-    echo "<br>";
-}
+echo '<table border="0" cellspacing="2" cellpadding="2"> 
+      <tr> 
+          <td> <font face="Arial">Manufacturer ID</font> </td> 
+          <td> <font face="Arial">Name</font> </td> 
+      </tr>';
+
+if ($result = $con->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["man_id"];
+        $field2name = $row["name"];
+
+        echo '<tr> 
+                  <td>'.$field1name.'</td> 
+                  <td>'.$field2name.'</td> 
+              </tr>';
+    }
 
 mysqli_close($con);
-?>
+}
