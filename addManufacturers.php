b553488d3a72641c52e346f4231d3754c80a1907
Ranjodh Sandhu <ranjodhsandhu1999@gmail.com>
2021-05-02 19:08:51 -0400

    Stores and Mans

---
+++ addManufacturers.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<?php
+// header begins body tag
+include('Header.php');
+echo "<br> <br> <br> <br>";
+
+// make the user login first 
+if (!isset($_SESSION['user'])) {
+    header('location: Home.php');
+}
+// if the user is logged in, display the home page content
+else {
+?>
+
+<?php
+    require('backend/db.php');
+    require('backend/manufacturers.php');
+    $status = "";
+    if (isset($_POST['new']) && $_POST['new'] == 1) {
+        $a = $_REQUEST['a'];
+        $b = $_REQUEST['b'];
+        $c = $_REQUEST['c'];
+
+        manufacturersInsert($a, $b, $c);
+
+        $status = "New Record Inserted Successfully.";
+        echo "<script> window.location.assign('Stores.php'); </script>";
+        $con->close();
+    }
+}
+?>
+
+<head>
+    <meta charset="utf-8">
+    <title>Insert New Manufacturer </title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <div class="form">
+        <h1>Insert New Manufacturer</h1>
+        <form name="form" method="post" action="">
+            <input type="hidden" name="new" value="1" />
+            <p><input type="text" name="a" placeholder="Manufacturer ID" required /></p>
+            <p><input type="text" name="b" placeholder="Name" required /></p>
+            <p><input type="text" name="c" placeholder="Phone" required /></p>
+
+            <p><input name="submit" type="submit" value="Submit" /></p>
+            <p style="color:#FF0000;"><?php echo $status; ?></p>
+    </div>
+    </div>
+    <script>
+        // Get the Sidebar
+        var mySidebar = document.getElementById("mySidebar");
+
+        // Get the DIV with overlay effect
+        var overlayBg = document.getElementById("myOverlay");
+
+        // Toggle between showing and hiding the sidebar, and add overlay effect
+        function w3_open() {
+            if (mySidebar.style.display === 'block') {
+                mySidebar.style.display = 'none';
+                overlayBg.style.display = "none";
+            } else {
+                mySidebar.style.display = 'block';
+                overlayBg.style.display = "block";
+            }
+        }
+
+        // Close the sidebar with the close button
+        function w3_close() {
+            mySidebar.style.display = "none";
+            overlayBg.style.display = "none";
+        }
+    </script>
+</body>
+
+</html>
